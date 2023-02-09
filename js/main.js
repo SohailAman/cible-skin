3a403d7c6e222984d06d19b17fe8b0b78894e79b
SohailAman <101714964+SohailAman@users.noreply.github.com>
2023-02-09 19:32:48 +0500

    Initail

--- js/main.js
+++ js/main.js
@@ -231,3 +231,90 @@ for (let i = 0; i < morePara.length; i++) {
   readMoreBtn[i].innerHTML = "En savoir plus";
 
 }
+
+
+// ---------slide text
+
+let selSlidesMain = document.querySelectorAll('.selebrites__slide');
+let selSlide1 = document.querySelector('.selebrites__slide1');
+let selSlide2 = document.querySelector('.selebrites__slide2');
+let selSlide3 = document.querySelector('.selebrites__slide3');
+let selSlide4 = document.querySelector('.selebrites__slide4');
+let selSlide5 = document.querySelector('.selebrites__slide5');
+let selCaptions1 = document.querySelector('.selebrites-caption1');
+let selCaptions2 = document.querySelector('.selebrites-caption2');
+let selCaptions3 = document.querySelector('.selebrites-caption3');
+let selCaptions4 = document.querySelector('.selebrites-caption4');
+let selCaptions5 = document.querySelector('.selebrites-caption5');
+let selName1 = document.querySelector('.selebrites__name1');
+let selName2 = document.querySelector('.selebrites__name2');
+let selName3 = document.querySelector('.selebrites__name3');
+let selName4 = document.querySelector('.selebrites__name4');
+let selName5 = document.querySelector('.selebrites__name5');
+
+function sel() {
+  if (selSlide1.classList.contains("is-active")) {
+    selCaptions1.classList.remove('remove');
+    selName1.classList.remove('remove');
+  }
+  else {
+
+    selCaptions1.classList.add('remove');
+    selName1.classList.add('remove');
+  }
+}
+setInterval(sel, 500);
+// -----
+function sel2() {
+  if (selSlide2.classList.contains("is-active")) {
+    selCaptions2.classList.remove('remove');
+    selName2.classList.remove('remove');
+  }
+  else {
+
+    selCaptions2.classList.add('remove');
+    selName2.classList.add('remove');
+  }
+}
+setInterval(sel2, 500);
+// -----
+function sel3() {
+  if (selSlide3.classList.contains("is-active")) {
+    selCaptions3.classList.remove('remove');
+    selName3.classList.remove('remove');
+  }
+  else {
+
+    selCaptions3.classList.add('remove');
+    selName3.classList.add('remove');
+  }
+}
+setInterval(sel3, 500);
+// -----
+function sel4() {
+  if (selSlide4.classList.contains("is-active")) {
+    selCaptions4.classList.remove('remove');
+    selName4.classList.remove('remove');
+  }
+  else {
+
+    selCaptions4.classList.add('remove');
+    selName4.classList.add('remove');
+  }
+}
+setInterval(sel4, 500);
+// -----
+function sel5() {
+  if (selSlide5.classList.contains("is-active")) {
+    selCaptions5.classList.remove('remove');
+    selName5.classList.remove('remove');
+  }
+  else {
+
+    selCaptions5.classList.add('remove');
+    selName5.classList.add('remove');
+  }
+}
+setInterval(sel5, 500);
+
+// ***********
