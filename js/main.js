6a3ae5a34e800b26375d17e6eb444ddd28d5b22e
SohailAman <101714964+SohailAman@users.noreply.github.com>
2023-02-14 14:43:07 +0500

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+"use sctrict";
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
@@ -7,24 +7,7 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelectorAll(".header__menu-btn");
 const headerEl = document.querySelector(".header");
 
-btnNavEl.forEach((btn) =>
-  btn.addEventListener("click", () => headerEl.classList.toggle("nav-open"))
-);
-const current = document.querySelector('#current');
-const imgs = document.querySelectorAll('.hero__gallery-container img');
-const opacity = 0.4;
-
-imgs[0].style.opacity = opacity;
-
-imgs.forEach(img => img.addEventListener('click', imgClick));
-
-function imgClick(e) {
-  imgs.forEach(img => (img.style.opacity = 1));
-  current.src = e.target.src;
-  current.classList.add('fade-in');
-  setTimeout(() => current.classList.remove('fade-in'), 500);
-  e.target.style.opacity = opacity;
-}
+btnNavEl.forEach((btn) => btn.addEventListener("click", () => headerEl.classList.toggle("nav-open")));
 
 const googleSplide = new Splide(".google__splide", {
   focus: "center",
@@ -60,48 +43,21 @@ const maisonSlider = new Splide(".maison__splide", {
   perPage: 1,
   perMove: 1,
   mediaQuery: "min",
-  fixedWidth: "206px",
-  width: "375px",
-  gap: "26px",
+  padding: { left: "3em", right: "4em" },
   breakpoints: {
     560: {
-      perPage: 1,
+      perPage: 2,
+      padding: { left: "5.2em", right: "5.2em" },
+      gap: "5rem",
     },
     1119: {
-      perPage: 1,
+      perPage: 3,
       padding: "0rem",
-      fixedWidth: "312px",
-      width: "650px",
+      width: "940px",
     },
   },
 });
 maisonSlider.mount();
-
-const maisonCulteSlider = new Splide(".maison-culte__splide", {
-  focus: "center",
-  type: "loop",
-  perPage: 1,
-  perMove: 1,
-  mediaQuery: "min",
-  width: "405px",
-  fixedWidth: "250px",
-  gap: "11px",
-  breakpoints: {
-    712: {
-      perPage: 2,
-
-      gap: "11px",
-    },
-    1119: {
-      perPage: 3,
-
-      fixedWidth: "340px",
-      width: "700px",
-      // gap: "10rem",
-    },
-  },
-});
-maisonCulteSlider.mount();
 
 const selebritesSplide = new Splide(".selebrites__slider", {
   type: "loop",
@@ -120,121 +76,17 @@ window.addEventListener("load", function (e) {
   body.classList.remove("global-preload");
 });
 
-// <!-- Initialize Swiper -->
-var swiper2 = new Swiper(".mySwiperCosmatic", {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-// <!-- Initialize Swiper -->
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    1440: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-  },
-});
-
-// <!-- Initialize Swiper -->
-var swiperSteps = new Swiper(".mySwiperSteps", {
-  slidesPerView: 1.2,
-  spaceBetween: 30,
-  centeredSlide: true,
-  navigation: {
-    nextEl: ".step-swiper-button-next",
-    prevEl: ".step-swiper-button-prev",
-  }, breakpoints: {
-    640: {
-      spaceBetween: 20,
-      slidesPerView: 2,
-    },
-    998: {
-      spaceBetween: 20,
-      slidesPerView: 3,
-    },
-  },
-});
-
-
-// -------
-$(window).scroll(function () {
-  if ($(this).scrollTop() > $(window).height() / 4) {
-    $(".reservation").addClass("fixed");
-  } else {
-    $(".reservation").removeClass("fixed");
-  }
-});
-
-$(window).scroll(function () {
-  let target = $(".footer").offset().top;
-  let currentScroll = $(target).scrollTop();
-
-  if (currentScroll >= target) {
-    $(".reservation").css({
-      position: "initial",
-      display: "none"
-    });
-  }
-});
-
-// Read more btns
-
-let readMoreBtn = document.querySelectorAll('.readMoreBtn');
-let elipsis = document.querySelectorAll('.elipsis');
-let morePara = document.querySelectorAll('.morePara');
-
-let viewed = false;
-
-for (let i = 0; i < morePara.length; i++) {
-
-  readMoreBtn[i].addEventListener('click', () => {
-    if (viewed === false) {
-      elipsis[i].style.display = "none";
-      morePara[i].classList.remove('hideText');
-      readMoreBtn[i].innerHTML = "Show less";
-      viewed = true;
-    }
-    else {
-      elipsis[i].style.display = "block";
-      morePara[i].classList.add('hideText');
-      readMoreBtn[i].innerHTML = "En savoir plus";
-      viewed = false;
-    }
-  })
-
-  elipsis[i].style.display = "block";
-  morePara[i].classList.add('hideText');
-  readMoreBtn[i].innerHTML = "En savoir plus";
-
-}
-
-
-// ---------slide text
-
+// const arrows = document.querySelectorAll(".maison__arrows");
+// arrows.forEach((arrow) => {
+//   arrow.addEventListener("click", (e) => {
+//     const maisonSliderList = document.querySelectorAll(".maison__slide");
+//     maisonSliderList.forEach((slide) => {
+//       if (slide.classList.contains("is-visible" && "is-active")) {
+//         console.log(12);
+//       }
+//     });
+//   });
+// });
 let selSlidesMain = document.querySelectorAll('.selebrites__slide');
 let selSlide1 = document.querySelector('.selebrites__slide1');
 let selSlide2 = document.querySelector('.selebrites__slide2');
@@ -264,7 +116,7 @@ function sel() {
   }
 }
 setInterval(sel, 500);
-// -----
+
 function sel2() {
   if (selSlide2.classList.contains("is-active")) {
     selCaptions2.classList.remove('remove');
@@ -277,7 +129,7 @@ function sel2() {
   }
 }
 setInterval(sel2, 500);
-// -----
+
 function sel3() {
   if (selSlide3.classList.contains("is-active")) {
     selCaptions3.classList.remove('remove');
@@ -290,7 +142,7 @@ function sel3() {
   }
 }
 setInterval(sel3, 500);
-// -----
+
 function sel4() {
   if (selSlide4.classList.contains("is-active")) {
     selCaptions4.classList.remove('remove');
@@ -303,7 +155,7 @@ function sel4() {
   }
 }
 setInterval(sel4, 500);
-// -----
+
 function sel5() {
   if (selSlide5.classList.contains("is-active")) {
     selCaptions5.classList.remove('remove');
@@ -317,4 +169,4 @@ function sel5() {
 }
 setInterval(sel5, 500);
 
-// ***********
+
